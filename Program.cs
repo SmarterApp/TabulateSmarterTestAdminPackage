1545a817a878eaf56c2202c84684e09dc14ddced
Brandt Redd <brandt@redd.org>
2015-10-01 10:09:23 -0600

    First Functional Version

---
+++ Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace TabulateSmarterTestAdminPackage
+{
+    public enum PackageType : int
+    {
+        Registration,
+        Administration,
+        Scoring,
+        Reporting
+    }
+
+    class Program
+    {
+
+        static string sSyntax =
+@"This tool tabulates the item metadata included in Smarter Balanced
+Test Administration packages. It can also be used with Test Scoring
+packages since they use the same format for the item sections.
+
+Multiple input packages may be specified in which case the output is
+the aggregation of all input files.
+
+By default the tool processes only test administration packages as
+determined by the @purpose attribute on the root <testspecification>
+element in the file. All other .xml files are skipped. The -scoring
+option will cause it to tabulate scoring packages and ignore all
+other package types.
+
+Command-line parameters:
+ -i <input filename>
+Specifies an input filename which should be the name of a test
+administration or test reporting package. The Filename may include
+wildcards in which case all matching files will be processed. If the
+filename is a .zip then all .xml files in the .zip will be processed
+regardless of their path within the .zip file. The -i parameter may be
+repeated to process multiple input sources.
+
+ -oi <output filename>
+Output file for item information. This will be a .csv formatted file that
+contains the item metadata from all of the input files.
+
+ -os <output filename>
+Output file for stimulus information. This will be a .csv formatted file
+that contains the stimulus metadata for all of the input files.
+
+-scoring
+Causes the tabulator to tabulate scoring packages and ignore all other
+package types. If this option is not specified, the tabulator tabulates
+all administration packages and ignores all other package types.
+";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> inputFilenames = new List<string>();
+                string oiFilename = null;
+                string osFilename = null;
+                PackageType packageType = PackageType.Administration;
+                
+                bool help = false;
+
+                for (int i=0; i<args.Length; ++i)
+                {
+                    switch (args[i])
+                    {
+                        case "-h":
+                            help = true;
+                            break;
+
+                        case "-i":
+                            {
+                                ++i;
+                                if (i >= args.Length) throw new ArgumentException("Invalid command line. '-i' option not followed by filename.");
+                                inputFilenames.Add(args[i]);
+                            }
+                            break;
+
+                        case "-oi":
+                            {
+                                ++i;
+                                if (i >= args.Length) throw new ArgumentException("Invalid command line. '-oi' option not followed by filename.");
+                                if (oiFilename != null) throw new ArgumentException("Only one item output file may be specified.");
+                                oiFilename = Path.GetFullPath(args[i]);
+                            }
+                            break;
+
+                        case "-os":
+                            {
+                                ++i;
+                                if (i >= args.Length) throw new ArgumentException("Invalid command line. '-os' option not followed by filename.");
+                                if (osFilename != null) throw new ArgumentException("Only one stimulus output file may be specified.");
+                                osFilename = Path.GetFullPath(args[i]);
+                            }
+                            break;
+
+                        case "-scoring":
+                            packageType = PackageType.Scoring;
+                            break;
+
+                        default:
+                            throw new ArgumentException(string.Format("Unknown command line option '{0}'. Use '-h' for syntax help.", args[i]));
+                    }
+                }
+
+                if (help || args.Length == 0)
+                {
+                    Console.WriteLine(sSyntax);
+                }
+
+                else
+                {
+                    if (inputFilenames.Count == 0 || (oiFilename == null && osFilename == null)) throw new ArgumentException("Invalid command line. One output filename and at least one input filename must be specified.");
+
+                    if (oiFilename != null)
+                        Console.WriteLine("Writing item metadata to: " + oiFilename);
+                    if (osFilename != null)
+                        Console.WriteLine("Writing stimulus metadata to: " + osFilename);
+                    using (TestPackageProcessor processor = new TestPackageProcessor(oiFilename, osFilename))
+                    {
+                        processor.ExpectedPackageType = packageType;
+
+                        foreach (string filename in inputFilenames)
+                        {
+                            ProcessInputFilename(filename, processor);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine();
+#if DEBUG
+                Console.WriteLine(err.ToString());
+#else
+                Console.WriteLine(err.Message);
+#endif
+            }
+
+            if (Win32Interop.ConsoleHelper.IsSoleConsoleOwner)
+            {
+                Console.Write("Press any key to exit.");
+                Console.ReadKey(true);
+            }
+        }
+
+        static void ProcessInputFilename(string filenamePattern, ITestResultProcessor processor)
+        {
+            int count = 0;
+            string directory = Path.GetDirectoryName(filenamePattern);
+            if (string.IsNullOrEmpty(directory)) directory = Environment.CurrentDirectory;
+            string pattern = Path.GetFileName(filenamePattern);
+            foreach (string filename in Directory.GetFiles(directory, pattern))
+            {
+                switch (Path.GetExtension(filename).ToLower())
+                {
+                    case ".xml":
+                        ProcessInputXmlFile(filename, processor);
+                        break;
+
+                    case ".zip":
+                        ProcessInputZipFile(filename, processor);
+                        break;
+
+                    default:
+                        throw new ArgumentException(string.Format("Input file '{0}' is of unsupported time. Only .xml and .zip are supported.", filename));
+                }
+                ++count;
+            }
+            if (count == 0) throw new ArgumentException(string.Format("Input file '{0}' not found!", filenamePattern));
+        }
+
+        static void ProcessInputXmlFile(string filename, ITestResultProcessor processor)
+        {
+            Console.WriteLine("Processing: " + filename);
+            using (FileStream stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                processor.ProcessResult(stream);
+            }
+            Console.WriteLine();
+        }
+
+        static void ProcessInputZipFile(string filename, ITestResultProcessor processor)
+        {
+            Console.WriteLine("Processing: " + filename);
+            using (ZipArchive zip = ZipFile.Open(filename, ZipArchiveMode.Read))
+            {
+                foreach(ZipArchiveEntry entry in zip.Entries)
+                {
+                    // Must not be folder (empty name) and must have .xml extension
+                    if (!string.IsNullOrEmpty(entry.Name) && Path.GetExtension(entry.Name).Equals(".xml", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("   Processing: " + entry.FullName);
+                        using (Stream stream = entry.Open())
+                        {
+                            processor.ProcessResult(stream);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    [Flags]
+    enum DIDFlags : int
+    {
+        None = 0,
+        Id = 1,             // Student ID
+        Name = 2,           // Student Name
+        Birthdate = 4,
+        Demographics = 8,   // Sex, Race, Ethnicity
+        School = 16          // School and districtID or ExternalSSID is unaffected
+    }
+
+    interface ITestResultProcessor : IDisposable
+    {
+        void ProcessResult(Stream input);
+    }
+}
---
+++ TestPackageProcessor.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.XPath;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace TabulateSmarterTestAdminPackage
+{
+
+    class TestPackageProcessor : ITestResultProcessor
+    {
+        enum ItemFieldNames : int
+        {
+            TestName,
+            TestSubject,
+            TestGrade,
+            TestType, // PT or Summative
+            ItemId, // Strip the "200-" bankId prefix
+            Filename,
+            Version,
+            ItemType,
+            Grade,
+            PassageRef,
+            PassageLength,
+            HearingImpaired,
+            ASL,
+            Braille,
+            LanguageBraille,
+            DOK,
+            Language,
+            Calculator,
+            Glossary,
+            ScoringEngine,
+            Spanish,
+            TDSPoolFilter,
+            MeasurementModel,
+            Weight,
+            ScorePoints,
+            a,
+            b0_b,
+            b1_c,
+            b2,
+            b3,
+            bpref1,
+            bpref2,
+            bpref3,
+            bpref4,
+            bpref5,
+            bpref6,
+            bpref7
+        }
+        static readonly int ItemFieldNamesCount = Enum.GetNames(typeof(ItemFieldNames)).Length;
+        const int MaxBpRefs = 7;
+
+        enum StimFieldNames : int
+        {
+            TestName,
+            TestSubject,
+            TestGrade,
+            TestType, // PT or Summative
+            StimId, // Strip the "200-" bankId prefix
+            Filename,
+            Version
+        };
+        static readonly int StimFieldNamesCount = Enum.GetNames(typeof(StimFieldNames)).Length;
+
+        // Test Info
+        static XPathExpression sXp_PackagePurpose = XPathExpression.Compile("/testspecification/@purpose");
+        static XPathExpression sXp_TestName = XPathExpression.Compile("/testspecification/identifier/@name");
+        static XPathExpression sXp_TestSubject = XPathExpression.Compile("/testspecification/property[@name='subject']/@value");
+        static XPathExpression sXp_TestGrade = XPathExpression.Compile("/testspecification/property[@name='grade']/@value");
+        static XPathExpression sXp_TestType = XPathExpression.Compile("/testspecification/property[@name='type']/@value");
+        
+        // Stim Selector
+        static XPathExpression sXp_Stim = XPathExpression.Compile("/testspecification/administration/itempool/passage");
+
+        // Item Selector
+        static XPathExpression sXp_Item = XPathExpression.Compile("/testspecification/administration/itempool/testitem");
+
+        // Item Info
+        static XPathExpression sXp_ItemId = XPathExpression.Compile("identifier/@uniqueid");
+        static XPathExpression sXp_Filename = XPathExpression.Compile("@filename");
+        static XPathExpression sXp_Version = XPathExpression.Compile("identifier/@version");
+        static XPathExpression sXp_ItemType = XPathExpression.Compile("@itemtype");
+        static XPathExpression sXp_PassageRef = XPathExpression.Compile("passageref");
+        static XPathExpression sXp_MeasurementModel = XPathExpression.Compile("itemscoredimension/@measurementmodel");
+        static XPathExpression sXp_Weight = XPathExpression.Compile("itemscoredimension/@weight");
+        static XPathExpression sXp_ScorePoints = XPathExpression.Compile("itemscoredimension/@scorepoints");
+        static XPathExpression sXp_ParameterName = XPathExpression.Compile("itemscoredimension/itemscoreparameter/@measurementparameter");
+        static XPathExpression sXp_Parameter1 = XPathExpression.Compile("itemscoredimension/itemscoreparameter[@measurementparameter='a']/@value");
+        static XPathExpression sXp_Parameter2 = XPathExpression.Compile("itemscoredimension/itemscoreparameter[@measurementparameter='a' or @measurementparameter='b0']/@value");
+        static XPathExpression sXp_Parameter3 = XPathExpression.Compile("itemscoredimension/itemscoreparameter[@measurementparameter='a' or @measurementparameter='b1']/@value");
+        static XPathExpression sXp_Parameter4 = XPathExpression.Compile("itemscoredimension/itemscoreparameter[@measurementparameter='b2']/@value");
+        static XPathExpression sXp_Parameter5 = XPathExpression.Compile("itemscoredimension/itemscoreparameter[@measurementparameter='b3']/@value");
+        static XPathExpression sXp_Bpref = XPathExpression.Compile("bpref");
+
+        // Pool Properties
+        static XPathExpression sXp_PoolProperty = XPathExpression.Compile("poolproperty");
+        static XPathExpression sXp_PPProperty = XPathExpression.Compile("@property");
+        static XPathExpression sXp_PPValue = XPathExpression.Compile("@value");
+
+        static Dictionary<string, int> sPoolPropertyMapping;
+        static HashSet<string> sKnownMeasurementModels;
+        static HashSet<string> sKnownMeasurementParameters;
+
+        static TestPackageProcessor()
+        {
+            sPoolPropertyMapping = new Dictionary<string, int>();
+            sPoolPropertyMapping.Add("--ITEMTYPE--", 0); // Value of zero means suppress
+            sPoolPropertyMapping.Add("Appropriate for Hearing Impaired", (int)ItemFieldNames.HearingImpaired);
+            sPoolPropertyMapping.Add("ASL", (int)ItemFieldNames.ASL);
+            sPoolPropertyMapping.Add("Braille", (int)ItemFieldNames.Braille);
+            sPoolPropertyMapping.Add("Depth of Knowledge", (int)ItemFieldNames.DOK);
+            sPoolPropertyMapping.Add("Grade", (int)ItemFieldNames.Grade);
+            sPoolPropertyMapping.Add("Language", (int)ItemFieldNames.Language);
+            sPoolPropertyMapping.Add("Scoring Engine", (int)ItemFieldNames.ScoringEngine);
+            sPoolPropertyMapping.Add("Spanish Translation", (int)ItemFieldNames.Spanish);
+            sPoolPropertyMapping.Add("Passage Length", (int)ItemFieldNames.PassageLength);
+            sPoolPropertyMapping.Add("TDSPoolFilter", (int)ItemFieldNames.TDSPoolFilter);
+            sPoolPropertyMapping.Add("Calculator", (int)ItemFieldNames.Calculator);
+            sPoolPropertyMapping.Add("Glossary", (int)ItemFieldNames.Glossary);
+
+            sKnownMeasurementModels = new HashSet<string>();
+            sKnownMeasurementModels.Add("RAWSCORE");
+            sKnownMeasurementModels.Add("IRT3PLn");
+            sKnownMeasurementModels.Add("IRTGPC");
+
+            sKnownMeasurementParameters = new HashSet<string>();
+            sKnownMeasurementParameters.Add("a");
+            sKnownMeasurementParameters.Add("b");
+            sKnownMeasurementParameters.Add("c");
+            sKnownMeasurementParameters.Add("b0");
+            sKnownMeasurementParameters.Add("b1");
+            sKnownMeasurementParameters.Add("b2");
+            sKnownMeasurementParameters.Add("b3");
+        }
+
+        Parse.CsvWriter m_iWriter;
+        Parse.CsvWriter m_sWriter;
+
+        public TestPackageProcessor(string oiFilename, string osFilename)
+        {
+#if !DEBUG
+            if (File.Exists(oiFilename)) throw new ApplicationException(string.Format("Output file, '{0}' already exists.", oiFilename));
+#endif
+            if (oiFilename != null)
+            {
+                m_iWriter = new Parse.CsvWriter(oiFilename, false);
+                m_iWriter.Write(Enum.GetNames(typeof(ItemFieldNames)));
+            }
+            if (osFilename != null)
+            {
+                m_sWriter = new Parse.CsvWriter(osFilename, false);
+                m_sWriter.Write(Enum.GetNames(typeof(StimFieldNames)));
+            }
+        }
+
+        public PackageType ExpectedPackageType { get; set; }
+
+        static readonly UTF8Encoding UTF8NoByteOrderMark = new UTF8Encoding(false);
+
+        public void ProcessResult(Stream input)
+        {
+            XPathDocument doc = new XPathDocument(input);
+            XPathNavigator nav = doc.CreateNavigator();
+
+            // Get the package purpose
+            string purpose = nav.Eval(sXp_PackagePurpose);
+            if (!string.Equals(purpose, ExpectedPackageType.ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("  Skipping package. Type is '{0}' but processing '{1}.", purpose, ExpectedPackageType);
+            }
+
+            // Get the test info
+            string testName = nav.Eval(sXp_TestName);
+            string testSubject = nav.Eval(sXp_TestSubject);
+            string testGrade = nav.Eval(sXp_TestGrade);
+            string testType = nav.Eval(sXp_TestType);
+
+            // Report the item fields
+            if (m_iWriter != null)
+            {
+                XPathNodeIterator nodes = nav.Select(sXp_Item);
+                while (nodes.MoveNext())
+                {
+                    XPathNavigator node = nodes.Current;
+
+                    // Collect the item fields
+                    string[] itemFields = new string[ItemFieldNamesCount];
+                    itemFields[(int)ItemFieldNames.TestName] = testName;
+                    itemFields[(int)ItemFieldNames.TestSubject] = testSubject;
+                    itemFields[(int)ItemFieldNames.TestGrade] = testGrade;
+                    itemFields[(int)ItemFieldNames.TestType] = testType;
+                    itemFields[(int)ItemFieldNames.ItemId] = Strip200(node.Eval(sXp_ItemId));
+                    itemFields[(int)ItemFieldNames.Filename] = node.Eval(sXp_Filename);
+                    itemFields[(int)ItemFieldNames.Version] = node.Eval(sXp_Version);
+                    itemFields[(int)ItemFieldNames.ItemType] = node.Eval(sXp_ItemType);
+                    itemFields[(int)ItemFieldNames.PassageRef] = Strip200(node.Eval(sXp_PassageRef));
+
+                    // Process PoolProperties
+                    List<string> glossary = new List<string>();
+                    XPathNodeIterator ppNodes = node.Select(sXp_PoolProperty);
+                    while (ppNodes.MoveNext())
+                    {
+                        XPathNavigator ppNode = ppNodes.Current;
+                        string ppProperty = ppNode.Eval(sXp_PPProperty).Trim();
+                        string ppValue = ppNode.Eval(sXp_PPValue).Trim();
+                        if (!string.IsNullOrEmpty(ppProperty) && !string.IsNullOrEmpty(ppValue))
+                        {
+                            // Special case for Braille language
+                            int fieldIndex;
+                            if (ppProperty.Equals("Language", StringComparison.Ordinal) && ppValue.Equals("ENU-Braille", StringComparison.Ordinal))
+                            {
+                                itemFields[(int)ItemFieldNames.LanguageBraille] = ppValue;
+                            }
+
+                            // Special case for Spanish language
+                            else if (ppProperty.Equals("Language", StringComparison.Ordinal) && ppValue.Equals("ESN", StringComparison.Ordinal))
+                            {
+                                itemFields[(int)ItemFieldNames.Spanish] = "Y";
+                            }
+
+                            // Special case for Spanish language
+                            else if (ppProperty.Equals("Spanish Translation", StringComparison.Ordinal))
+                            {
+                                itemFields[(int)ItemFieldNames.Spanish] = ppValue;
+                            }
+
+                            // Special case for Glossary
+                            else if (ppProperty.Equals("Glossary", StringComparison.Ordinal))
+                            {
+                                glossary.Add(ppValue);
+                            }
+
+                            else if (sPoolPropertyMapping.TryGetValue(ppProperty, out fieldIndex))
+                            {
+                                if (fieldIndex != 0)
+                                {
+                                    if (!string.IsNullOrEmpty(itemFields[fieldIndex]))
+                                    {
+                                        Console.WriteLine("  '{0}={1}' Multiple values for pool property", ppProperty, ppValue);
+#if DEBUG
+                                        Debug.Fail(string.Format("  '{0}={1}' Multiple values for pool property", ppProperty, ppValue));
+#endif
+                                    }
+                                    itemFields[fieldIndex] = ppValue;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("  '{0}={1}' Unrecognized Pool Property", ppProperty, ppValue);
+#if DEBUG
+                                Debug.Fail(string.Format("  '{0}={1}' Unrecognized Pool Property", ppProperty, ppValue));
+#endif
+                            }
+                        }
+                    }
+                    glossary.Sort();
+                    itemFields[(int)ItemFieldNames.Glossary] = string.Join(";", glossary);
+
+                    itemFields[(int)ItemFieldNames.MeasurementModel] = node.Eval(sXp_MeasurementModel);
+                    itemFields[(int)ItemFieldNames.Weight] = FormatDouble(node.Eval(sXp_Weight));
+                    itemFields[(int)ItemFieldNames.ScorePoints] = node.Eval(sXp_ScorePoints);
+                    itemFields[(int)ItemFieldNames.a] = FormatDouble(node.Eval(sXp_Parameter1));
+                    itemFields[(int)ItemFieldNames.b0_b] = FormatDouble(node.Eval(sXp_Parameter2));
+                    itemFields[(int)ItemFieldNames.b1_c] = FormatDouble(node.Eval(sXp_Parameter3));
+                    itemFields[(int)ItemFieldNames.b2] = FormatDouble(node.Eval(sXp_Parameter4));
+                    itemFields[(int)ItemFieldNames.b3] = FormatDouble(node.Eval(sXp_Parameter5));
+
+                    // Check known measurement model
+                    if (!sKnownMeasurementModels.Contains(itemFields[(int)ItemFieldNames.MeasurementModel]))
+                    {
+                        Console.WriteLine("  '{0}' Unrecognized Measurement Model", itemFields[(int)ItemFieldNames.MeasurementModel]);
+#if DEBUG
+                        Debug.Fail(string.Format("  '{0}' Unrecognized Measurement Model", itemFields[(int)ItemFieldNames.MeasurementModel]));
+#endif
+                    }
+
+                    // Check known parameters
+                    XPathNodeIterator pnNodes = node.Select(sXp_ParameterName);
+                    while (pnNodes.MoveNext())
+                    {
+                        string name = pnNodes.Current.Value;
+                        if (!sKnownMeasurementParameters.Contains(name))
+                        {
+                        Console.WriteLine("  '{0}' Unrecognized Measurement Parameter", name);
+#if DEBUG
+                        Debug.Fail(string.Format("  '{0}' Unrecognized Measurement Parameter", name));
+#endif
+                        }
+                    }
+
+                    // bprefs
+                    int bpIndex = 0;
+                    XPathNodeIterator bpNodes = node.Select(sXp_Bpref);
+                    while (bpNodes.MoveNext())
+                    {
+                        string bpref = bpNodes.Current.Value;
+                        if (bpIndex >= MaxBpRefs)
+                        {
+                            Console.WriteLine("  More than {0} bpref nodes", MaxBpRefs);
+#if DEBUG
+                            Debug.Fail(string.Format("  More than {0} bpref nodes", MaxBpRefs));
+#endif
+
+                        }
+                        itemFields[(int)ItemFieldNames.bpref1 + bpIndex++] = bpref;
+                    }
+
+                    // Write one line to the CSV
+                    m_iWriter.Write(itemFields);
+                }
+            }
+
+            // Report the stimuli fields
+            if (m_sWriter != null)
+            {
+                XPathNodeIterator nodes = nav.Select(sXp_Stim);
+                while (nodes.MoveNext())
+                {
+                    XPathNavigator node = nodes.Current;
+
+                    // Collect the stim fields
+                    string[] stimFields = new string[StimFieldNamesCount];
+                    stimFields[(int)StimFieldNames.TestName] = testName;
+                    stimFields[(int)StimFieldNames.TestSubject] = testSubject;
+                    stimFields[(int)StimFieldNames.TestGrade] = testGrade;
+                    stimFields[(int)StimFieldNames.TestType] = testType;
+                    stimFields[(int)StimFieldNames.StimId] = Strip200(node.Eval(sXp_ItemId));
+                    stimFields[(int)StimFieldNames.Filename] = node.Eval(sXp_Filename);
+                    stimFields[(int)StimFieldNames.Version] = node.Eval(sXp_Version);
+
+                    // Write one line to the CSV
+                    m_sWriter.Write(stimFields);
+                }
+            }
+
+        } // ProcessPackage
+
+        static string Strip200(string val)
+        {
+            return val.StartsWith("200-", StringComparison.Ordinal) ? val.Substring(4) : val;
+        }
+
+        static string FormatDouble(string val)
+        {
+            if (string.IsNullOrEmpty(val)) return string.Empty;
+            double v;
+            return (double.TryParse(val, out v)) ? v.ToString("G", System.Globalization.CultureInfo.InvariantCulture) : val;
+        }
+
+        ~TestPackageProcessor()
+        {
+            Dispose(false);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        void Dispose(bool disposing)
+        {
+            if (m_iWriter != null)
+            {
+#if DEBUG
+                if (!disposing) Debug.Fail("Failed to dispose CsvWriter");
+#endif
+                m_iWriter.Dispose();
+                m_iWriter = null;
+            }
+            if (m_sWriter != null)
+            {
+                m_sWriter.Dispose();
+                m_sWriter = null;
+            }
+            if (disposing)
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+
+    static class XPathNavitagorHelper
+    {
+        public static string Eval(this XPathNavigator nav, XPathExpression expression)
+        {
+            if (expression.ReturnType == XPathResultType.NodeSet)
+            {
+                XPathNodeIterator nodes = nav.Select(expression);
+                if (nodes.MoveNext())
+                {
+                    return nodes.Current.ToString();
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                return nav.Evaluate(expression).ToString();
+            }
+        }
+    }
+}
