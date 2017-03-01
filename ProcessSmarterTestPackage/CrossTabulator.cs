9ee1733c7f95434203fdd567dd80c0212bc5baf8
Gregory White <gwhite@fairwaytech.com>
2017-02-28 16:11:16 -0800

    New error type and harness for comparison between packages

--- ProcessSmarterTestPackage/CrossTabulator.cs
+++ ProcessSmarterTestPackage/CrossTabulator.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ProcessSmarterTestPackage.Processors.Common;
+using SmarterTestPackage.Common.Data;
+using ValidateSmarterTestPackage.RestrictedValues.Enums;
 
 namespace ProcessSmarterTestPackage
 {
@@ -29,6 +32,40 @@ namespace ProcessSmarterTestPackage
                     processor
                 });
             }
+        }
+
+        public List<CrossPackageValidationError> ExecuteValidation()
+        {
+            var result = new List<CrossPackageValidationError>();
+            foreach (var key in TestPackages.Keys)
+            {
+                if (TestPackages[key] == null || !TestPackages[key].Any())
+                {
+                    continue;
+                }
+                var adminPackage = TestPackages[key].FirstOrDefault(x => x.PackageType == PackageType.Administration);
+                var scoringPackage = TestPackages[key].FirstOrDefault(x => x.PackageType == PackageType.Scoring);
+
+                if (adminPackage == null)
+                {
+                    result.AddRange(CrossValidate(scoringPackage));
+                }
+                if (scoringPackage == null)
+                {
+                    result.AddRange(CrossValidate(adminPackage));
+                }
+                else
+                {
+                    result.AddRange(CrossValidate(adminPackage, scoringPackage));
+                }
+            }
+            return result;
+        }
+
+        public List<CrossPackageValidationError> CrossValidate(TestSpecificationProcessor primary,
+            TestSpecificationProcessor secondary = null)
+        {
+            return null;
         }
     }
 }
