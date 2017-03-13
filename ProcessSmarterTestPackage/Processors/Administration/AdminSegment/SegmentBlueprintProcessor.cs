2d44c03073a2be323dce9387c9cb9368e90584f7
Gregory White <gwhite@fairwaytech.com>
2017-03-13 15:26:21 -0700

    SMD-52: Added segment blueprint post-processor with validations around minopitems and maxopitems

--- ProcessSmarterTestPackage/Processors/Administration/AdminSegment/SegmentBlueprintProcessor.cs
+++ ProcessSmarterTestPackage/Processors/Administration/AdminSegment/SegmentBlueprintProcessor.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.XPath;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.XPath;
+using ProcessSmarterTestPackage.PostProcessors;
 using ProcessSmarterTestPackage.Processors.Common;
 using SmarterTestPackage.Common.Data;
 using SmarterTestPackage.Common.Extensions;
@@ -12,6 +15,11 @@ namespace ProcessSmarterTestPackage.Processors.Administration.AdminSegment
         {
             Navigator.GenerateList("segmentbpelement")
                 .ForEach(x => Processors.Add(new SegmentBlueprintElementProcessor(x, packageType)));
+        }
+
+        public override List<ValidationError> AdditionalValidations()
+        {
+            return new SegmentBlueprintPostProcessor(PackageType, this).GenerateErrors().ToList();
         }
     }
 }
