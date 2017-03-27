22b36935966bad098050bed99abe00fd92f9062b
Gregory White <gwhite@fairwaytech.com>
2017-03-27 13:38:55 -0700

    Added single-digit numbered grades to allowed list for possible grade values (not strictly spec, but functionally OK)

--- ValidateSmarterTestPackage/RestrictedValues/RestrictedList/RestrictedList.cs
+++ ValidateSmarterTestPackage/RestrictedValues/RestrictedList/RestrictedList.cs
@@ -36,7 +36,17 @@ namespace ValidateSmarterTestPackage.RestrictedValues.RestrictedList
                         "ASE",
                         "AdultESL",
                         "UG",
-                        "Other"
+                        "Other",
+                        // These aren't strictly spec, but functionally OK
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5",
+                        "6",
+                        "7",
+                        "8",
+                        "9"
                     }
                 },
                 {
